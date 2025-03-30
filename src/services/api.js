180bd1971c5c1a17b07fcf20240ad95dbe3a88e6
Gopinath <amanplayz2004@gmail.com>
2025-03-30 12:47:20 +0530

    final

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'https://firstreference.in/api';
 
 export const uploadCandidate = async (formData) => {
     try {
